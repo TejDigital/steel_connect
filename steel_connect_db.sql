8e61023478833e32bf41f09d0bf419ef63f7d9dc
Rohan <tejpratap.digitalshakha@gmail.com>
2023-09-08 16:51:57 +0530

    fri

--- steel_connect_db.sql
+++ steel_connect_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 06, 2023 at 10:57 AM
+-- Generation Time: Sep 08, 2023 at 01:21 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -52,6 +52,7 @@ CREATE TABLE `confirm_ticket_tbl` (
   `name` varchar(30) NOT NULL,
   `mobile` varchar(15) NOT NULL,
   `email` varchar(50) NOT NULL,
+  `tic_name` varchar(20) NOT NULL,
   `tic_price` int(12) NOT NULL,
   `payment_status` varchar(10) NOT NULL,
   `payment_id` varchar(100) NOT NULL,
@@ -63,18 +64,68 @@ CREATE TABLE `confirm_ticket_tbl` (
 -- Dumping data for table `confirm_ticket_tbl`
 --
 
-INSERT INTO `confirm_ticket_tbl` (`id`, `name`, `mobile`, `email`, `tic_price`, `payment_status`, `payment_id`, `tic_number`, `created_at`) VALUES
-(1, 'ABC Kumar', '1234567890', 'tejpratap.digitalshakha@gmail.com', 7079, 'complete', 'pay_MYjlIvD4eKuPT7', 3813, '2023-09-05 17:30:06'),
-(2, 'ashutosh', '1234567890', 'guptaashutosh957@gmail.com', 58999, 'complete', 'pay_MYvyMxrm7eME14', 2346, '2023-09-06 05:26:58'),
-(3, 'ashutosh', '8765432123', 'guptaashutosh957@gmail.com', 58999, 'complete', 'pay_MYw1grgkQX2wBV', 2346, '2023-09-06 05:30:03'),
-(4, 'ashutosh Gupta', '1234567890', 'guptaaashutosh957@gmail.com', 58999, 'complete', 'pay_MYw3h8E6BjD8W7', 2346, '2023-09-06 05:32:00'),
-(5, 'EARLY DELEGATE', '1234567890', 'tpsahu0711@gmail.com', 58999, 'complete', 'pay_MYw6E739vY4KL1', 2346, '2023-09-06 05:34:22'),
-(6, 'ABC Kumar', '1234567890', 'tejpratapsahu00@gmail.com', 58999, 'complete', 'pay_MYwBEnA4Q9vcw6', 3290, '2023-09-06 05:39:05'),
-(7, 'ashutosh', '1234567890', 'guptaaashutosh957@gmail.com', 58999, 'complete', 'pay_MYwCTklzcffHNb', 1476, '2023-09-06 05:40:18'),
-(8, 'ABC Kumar', '8765432123', 'aashutosh.digitalshakha@gmail.com', 58999, 'complete', 'pay_MYwG2gxdbkC7Cn', 2017, '2023-09-06 05:43:41'),
-(9, 'test id', '1234567890', 'tejpratapsahu00@gmail.com', 58999, 'complete', 'pay_MYwRIa1PTf81Oo', 4117, '2023-09-06 05:53:51'),
-(10, 'aashutosh test id', '9644477950', 'aashutosh.digitalshakha@gmail.com', 58999, 'complete', 'pay_MYwVj92eO6LzxZ', 3654, '2023-09-06 05:58:27'),
-(11, 'Saurabh Test Id', '9691699563', 'saurabhdeshmukh.pro@gmail.com', 58999, 'complete', 'pay_MYzCKdqdP1Iz8h', 3216, '2023-09-06 08:35:17');
+INSERT INTO `confirm_ticket_tbl` (`id`, `name`, `mobile`, `email`, `tic_name`, `tic_price`, `payment_status`, `payment_id`, `tic_number`, `created_at`) VALUES
+(1, 'ABC Kumar', '1234567890', 'tejpratap.digitalshakha@gmail.com', '', 7079, 'complete', 'pay_MYjlIvD4eKuPT7', 3813, '2023-09-05 17:30:06'),
+(2, 'ashutosh', '1234567890', 'guptaashutosh957@gmail.com', '', 58999, 'complete', 'pay_MYvyMxrm7eME14', 2346, '2023-09-06 05:26:58'),
+(3, 'ashutosh', '8765432123', 'guptaashutosh957@gmail.com', '', 58999, 'complete', 'pay_MYw1grgkQX2wBV', 2346, '2023-09-06 05:30:03'),
+(4, 'ashutosh Gupta', '1234567890', 'guptaaashutosh957@gmail.com', '', 58999, 'complete', 'pay_MYw3h8E6BjD8W7', 2346, '2023-09-06 05:32:00'),
+(5, 'EARLY DELEGATE', '1234567890', 'tpsahu0711@gmail.com', '', 58999, 'complete', 'pay_MYw6E739vY4KL1', 2346, '2023-09-06 05:34:22'),
+(6, 'ABC Kumar', '1234567890', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MYwBEnA4Q9vcw6', 3290, '2023-09-06 05:39:05'),
+(7, 'ashutosh', '1234567890', 'guptaaashutosh957@gmail.com', '', 58999, 'complete', 'pay_MYwCTklzcffHNb', 1476, '2023-09-06 05:40:18'),
+(8, 'ABC Kumar', '8765432123', 'aashutosh.digitalshakha@gmail.com', '', 58999, 'complete', 'pay_MYwG2gxdbkC7Cn', 2017, '2023-09-06 05:43:41'),
+(9, 'test id', '1234567890', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MYwRIa1PTf81Oo', 4117, '2023-09-06 05:53:51'),
+(10, 'aashutosh test id', '9644477950', 'aashutosh.digitalshakha@gmail.com', '', 58999, 'complete', 'pay_MYwVj92eO6LzxZ', 3654, '2023-09-06 05:58:27'),
+(11, 'Saurabh Test Id', '9691699563', 'saurabhdeshmukh.pro@gmail.com', '', 58999, 'complete', 'pay_MYzCKdqdP1Iz8h', 3216, '2023-09-06 08:35:17'),
+(12, 'ABC Kumar', '8765432123', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZ04GURcDvGZJL', 2853, '2023-09-06 09:27:17'),
+(13, 'EARLY DELEGATE', '1234567890', 'abc@gmail.com', '', 7079, 'complete', 'pay_MZ0DYqVIjxWECc', 1000, '2023-09-06 09:35:56'),
+(14, 'ABC Kumar', '8765432123', 'ashu@gmail.com', '', 7079, 'complete', 'pay_MZ0K3mj4IsZlGP', 1000, '2023-09-06 09:42:11'),
+(15, 'EARLY DELEGATE', '1234567890', 'ashu@gmail.com', '', 7079, 'complete', 'pay_MZ0Mk7TmhHlEVm', 1000, '2023-09-06 09:44:45'),
+(16, 'ABC Kumar', '1234567890', 'tejpratapsahu00@gmail.com', '', 7079, 'complete', 'pay_MZ0OhL7sDFEqT9', 1000, '2023-09-06 09:46:34'),
+(17, 'ABC Kumar', '1234567890', 'abc@gmail.com', '', 7079, 'complete', 'pay_MZ0TLA7TjicqcZ', 1000, '2023-09-06 09:51:01'),
+(18, 'rohan', '8765432123', 'admin@gmail.com', '', 7079, 'complete', 'pay_MZ0W5XET92Cp31', 1000, '2023-09-06 09:53:32'),
+(19, 'EARLY DELEGATE', '8765432123', 'abc@gmail.com', '', 7079, 'complete', 'pay_MZ0Z48uu8ZFCFU', 1000, '2023-09-06 09:56:26'),
+(20, 'EARLY DELEGATE', '1234567890', 'ashu@gmail.com', '', 7079, 'complete', 'pay_MZ0bTfyn33bLXn', 1000, '2023-09-06 09:58:43'),
+(21, 'ABC Kumar', '1234567890', 'ashu@gmail.com', '', 7079, 'complete', 'pay_MZ0cWXk1pHzt2W', 1000, '2023-09-06 09:59:39'),
+(22, 'EARLY DELEGATE', '8765432123', 'ashu@gmail.com', '', 7079, 'complete', 'pay_MZ0igxA7YVVvKf', 1000, '2023-09-06 10:05:34'),
+(23, 'ABC Kumar', '8765432123', 'rohan@gmail.com', '', 7079, 'complete', 'pay_MZ16mZEFvYthKb', 1000, '2023-09-06 10:28:20'),
+(24, 'ABC Kumar', '8765432123', 'ashu@gmail.com', '', 7079, 'complete', 'pay_MZ1vhLhh3WTvas', 1000, '2023-09-06 11:16:35'),
+(25, 'rohan', '8765432123', 'abc@gmail.com', '', 7079, 'complete', 'pay_MZ1yeWrbxMPImK', 1000, '2023-09-06 11:19:22'),
+(26, 'ABC Kumar', '8765432123', 'tejpratapsahu00@gmail.com', '', 7079, 'complete', 'pay_MZ21O4r6qF0nEp', 1000, '2023-09-06 11:21:55'),
+(27, 'EARLY DELEGATE', '8765432123', 'ashu@gmail.com', '', 7079, 'complete', 'pay_MZ28AZOT8Jxpv7', 1000, '2023-09-06 11:28:22'),
+(28, 'ABC Kumar', '8765432123', 'new@gmail.com', '', 7079, 'complete', 'pay_MZ2Yqv02wIr1q2', 1000, '2023-09-06 11:53:37'),
+(29, 'ABC Kumar', '1234567890', 'abc@gmail.com', '', 7079, 'complete', 'pay_MZ2ZqLc3qAdvvY', 1000, '2023-09-06 11:54:34'),
+(30, 'ABC ', '8765432123', 'abc@gmail.com', '', 7079, 'complete', 'pay_MZ2dyOj7yaVRFs', 1000, '2023-09-06 11:58:29'),
+(31, 'EARLY DELEGATE', '8765432123', 'abc@gmail.com', '', 7079, 'complete', 'pay_MZ2fFzfZlq64ex', 1000, '2023-09-06 11:59:39'),
+(32, 'ABC Kumar', '1234567890', 'new@gmail.com', '', 7079, 'complete', 'pay_MZ2jbssTeTI9Ii', 1000, '2023-09-06 12:03:50'),
+(33, 'ABC Kumar', '8765432123', 'ashu@gmail.com', '', 7079, 'complete', 'pay_MZ2l0AlWNV04q9', 1000, '2023-09-06 12:05:09'),
+(34, 'EARLY DELEGATE', '8765432123', 'admin@gmail.com', '', 7079, 'complete', 'pay_MZ2m9Ua2s7LJHr', 1000, '2023-09-06 12:06:14'),
+(35, 'ABC Kumar', '8765432123', 'new@gmail.com', '', 7079, 'complete', 'pay_MZ2nIKyzfdyUre', 1000, '2023-09-06 12:07:05'),
+(36, 'ABC Kumar', '8765432123', 'ashu@gmail.com', '', 7079, 'complete', 'pay_MZ2qyyxFUIuag6', 1000, '2023-09-06 12:10:49'),
+(37, 'EARLY DELEGATE', '8765432123', 'ashu@gmail.com', '', 7079, 'complete', 'pay_MZ2uDSXGmAOlvQ', 1000, '2023-09-06 12:13:50'),
+(38, 'rohan', '8765432123', 'ashu@gmail.com', '', 7079, 'complete', 'pay_MZ2xD3g4xi5s8E', 1000, '2023-09-06 12:16:42'),
+(39, 'ABC ', '1234567890', 'abc@gmail.com', '', 7079, 'complete', 'pay_MZ37LMWwLbf2Fv', 1000, '2023-09-06 12:26:14'),
+(40, 'tejpratap test id', '6260131302', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZk99D5wZWCKWE', 7462, '2023-09-08 06:31:35'),
+(41, 'ABC ', '8765432123', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZkRJ03phQkbFy', 7462, '2023-09-08 06:48:46'),
+(42, 'ABC Kumar', '8765432123', 'rohan@gmail.com', '', 58999, 'Pending', '', 2252, '2023-09-08 06:52:05'),
+(43, 'ABC ', '8765432123', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZkUmppdTAtw6a', 2252, '2023-09-08 06:52:16'),
+(44, 'ABC Kumar', '8765432123', 'tejpratapsahu00@gmail.com', '', 8259, 'complete', 'pay_MZm9EF6arSEoAK', 4162, '2023-09-08 08:29:18'),
+(45, 'rohan', '6260131302', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZmD2dgzn9t7Qh', 8672, '2023-09-08 08:32:49'),
+(46, 'Ashu ', '8976543567', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZmGA4zIdeTEZf', 8672, '2023-09-08 08:35:49'),
+(47, 'rohan', '8765432123', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZmJDG9me1Hv01', 8672, '2023-09-08 08:38:42'),
+(48, 'EARLY DELEGATE', '1234567890', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZmXY8xoqej8u0', 8672, '2023-09-08 08:52:20'),
+(49, 'rohan', '8765432123', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZmb89mm1ItXvF', 8672, '2023-09-08 08:55:41'),
+(50, 'rohan', '1234567890', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZnJuz6VTopMyS', 8672, '2023-09-08 09:38:08'),
+(51, 'EARLY DELEGATE', '8765432123', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZnNefyUXvxgUM', 8672, '2023-09-08 09:41:42'),
+(52, 'rohan', '6260131302', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZnWxaqWTkQvfr', 8672, '2023-09-08 09:50:28'),
+(53, 'ABC Kumar', '8765432123', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZnccQmVFXdkxf', 8672, '2023-09-08 09:55:49'),
+(54, 'rohan', '1234567890', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZne4AHCok9U82', 8672, '2023-09-08 09:57:07'),
+(55, 'ABC Kumar', '1234567890', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZoGcHKI1BPa22', 8672, '2023-09-08 10:33:38'),
+(56, 'rohan', '8765432123', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZoMyXPTxipkeY', 8672, '2023-09-08 10:39:41'),
+(57, 'new', '8765432123', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZoQQxjw9DvFXS', 8672, '2023-09-08 10:42:49'),
+(58, 'rohan', '8765432123', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZoS4MumB3AeKH', 8672, '2023-09-08 10:44:32'),
+(59, 'rohan', '1234567890', 'tejpratapsahu00@gmail.com', '', 58999, 'complete', 'pay_MZoTZquiWOtdQM', 8672, '2023-09-08 10:45:54'),
+(60, 'Bhoj Kumar Test ID', '1234567890', 'bhojkumar.digitalshakha@gmail.com', '', 58999, 'complete', 'pay_MZoXSjL8jXrioG', 6056, '2023-09-08 10:49:11'),
+(61, 'rohan test id', '8765432123', 'tejpratapsahu00@gmail.com', 'EXHIBITION', 58999, 'complete', 'pay_MZp0YPsjNF2feA', 4414, '2023-09-08 11:17:00');
 
 -- --------------------------------------------------------
 
@@ -350,7 +401,7 @@ ALTER TABLE `brochure_tbl`
 -- AUTO_INCREMENT for table `confirm_ticket_tbl`
 --
 ALTER TABLE `confirm_ticket_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT for table `contact_tbl`
